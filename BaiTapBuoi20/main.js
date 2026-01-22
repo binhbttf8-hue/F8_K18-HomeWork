daac746071c5bee07beb6755ff31f02afa748905
binhbui <binhbttf8@fullstack.edu.vn>
2026-01-22 17:12:49 +0700

    lesson 20 part 1

---
+++ BaiTapBuoi20/main.js
@@ -0,0 +1,20 @@
+let name = "Nguyen Van A";
+let age = 20;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+let a = 5;
+let b = 10;
+
+console.log(a);
+console.log(b);
+
+// Thay doi gia tri a va b
+a = 10;
+b = 5;
+ console.log("a=",a);
+ console.log("b=",b);
+
